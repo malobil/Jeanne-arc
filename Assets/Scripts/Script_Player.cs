14772ebf7fb51008875f28627a0bf3b2d096b649
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-04-02 18:32:14 +0200

    cinemachine + scene proto + script player

---
+++ Assets/Scripts/Script_Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player : MonoBehaviour
+{
+    private Rigidbody rb { get { return GetComponent<Rigidbody>(); } }
+    private float f_moveent_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float f_horizontal_input = Input.GetAxis("Horizontal");
+        rb.velocity = (transform.forward * f_horizontal_input) * f_moveent_speed * Time.fixedDeltaTime;
+    }
+}
